fb503c13f77c0fe97c8557dfd42e5cb418068ddd
Noel Varghese <91435868+NoelVarghese2006@users.noreply.github.com>
2025-06-28 17:15:31 -0500

    Update api.py

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ books = [
 @app.route('/api', methods=['POST'])
 def add_book():
     x = request.json
-    return predicting.predict(x['person_age'], x['person_income'], x['person_home_ownership'], x['person_emp_length'], x['loan_intent'], x['loan_grade'], x['loan_amnt'], x['loan_int_rate'], x['loan_percent_income'], x['cb_person_default_on_file'], x['cb_person_cred_hist_length']), 201
+    return jsonify({"result": predicting.predict(x['person_age'], x['person_income'], x['person_home_ownership'], x['person_emp_length'], x['loan_intent'], x['loan_grade'], x['loan_amnt'], x['loan_int_rate'], x['loan_percent_income'], x['cb_person_default_on_file'], x['cb_person_cred_hist_length'])}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
